995eac5b9bf83690fe6860e361520e2ae07d4a45
Josh Fleenor <joshsfleenor@gmail.com>
2018-11-20 10:34:38 -0500

    Org Files

---
+++ script.js
@@ -0,0 +1,17 @@
+// write the click part here or the "program if you will"
+
+// user_rock
+
+// user_paper
+
+// user_scissors
+
+// comp_rock
+
+// comp_paper
+
+// comp_scissors
+
+$(".user_button").click(function() {
+	$(this).css('background-color', 'pink');
+});
